a4f2fa93c1298cc5e318bd053420546a39e8d83d
Naveen Goyal <142402461+nstlnaveen@users.noreply.github.com>
2023-08-20 16:44:46 +0530

    Reverting extra directoties

--- devopsPOC/README.md
+++
@@ -1,3 +0,0 @@
-# devops_poc
-To Run the Jar file use this command
-java -cp devops_poc-0.2.0.jar hello.HelloWorld
--- devopsPOC/src/main/java/hello/Newgen.java
+++
@@ -1,7 +0,0 @@
-package hello;
-
-public class Newgen {
-    public String sayHello() {
-        return "DevOps Project for Newgen Technologies";
-    }
-}
